31b7de367eeb6b89f3a2f5f66117edd5dc9b155e
arvel <arvelbudion01@gmail.com>
2023-12-01 15:20:12 +0700

    could upload img in bucket

--- controllers/Fruits.controller.js
+++ controllers/Fruits.controller.js
@@ -1,41 +1,73 @@
-const fruits = require('../models/Fruits.model');
+const { Storage } = require("@google-cloud/storage");
 
-const getFruitsHistory = ((req,res) => {
-    res.json(fruits);
+const storage = new Storage({
+  projectId: "composed-card-399614",
+  keyFilename: "composed-card-399614-a52cf6699d3a.json",
 });
 
-const getFruit = ((req,res) => {
-    const id = parseInt(req.params.id);
-    const fruit = fruits.filter(f => f.id === id );
-    res.status(200).send(fruit);
-})
+const bucketName = "example-upload-fruit-image";
+const bucket = storage.bucket(bucketName);
 
-const addFruit = ((req,res) => {
-    const { ripeness, category, date, image, bookmark } = req.body;
+const fruits = require("../models/Fruits.model");
 
-    const newFruit = {
-        id: fruits.length + 1,
-        ripeness: ripeness,
-        category: category,
-        date: date,
-        image: image,
-        bookmark: bookmark,
+const getFruitsHistory = (req, res) => {
+  res.json(fruits);
+};
+
+const getFruit = (req, res) => {
+  const id = parseInt(req.params.id);
+  const fruit = fruits.filter((f) => f.id === id);
+  res.status(200).send(fruit);
+};
+
+const addFruit = (req, res) => {
+  const { ripeness, category, date, image, bookmark } = req.body;
+
+  const newFruit = {
+    id: fruits.length + 1,
+    ripeness: ripeness,
+    category: category,
+    date: date,
+    image: image,
+    bookmark: bookmark,
+  };
+
+  fruits.push(newFruit);
+  res.status(200).send("Fruit added!");
+};
+
+const deleteFruit = (req, res) => {
+  const id = req.params.id;
+  const index = fruits.findIndex((fruit) => fruit.id === id);
+  fruits.splice(index, 1);
+  res.status(200).send("Fruit deleted!");
+};
+
+const uploadToBucket = (req,res) => {
+    console.log("Made it /upload");
+    try {
+        if(req.file){
+            console.log("File found");
+            const bucketFile = bucket.file(`img/${req.file.originalname}`);
+            const bucketFileStream = bucketFile.createWriteStream();
+
+            bucketFileStream.on("finish", () => {
+                res.status(200).send("Success!");
+                console.log("success");
+            });
+            bucketFileStream.end(req.file.buffer);
+        } else {
+            throw "error with image";
+        }
+    } catch (error) {
+        res.status(500).send(error);
     }
-
-    fruits.push(newFruit);
-    res.status(200).send('Fruit added!');
-})
-
-const deleteFruit = ((req,res) => {
-    const id = req.params.id;
-    const index = fruits.findIndex(fruit => fruit.id === id);
-    fruits.splice(index,1)
-    res.status(200).send('Fruit deleted!')
-})
+}
 
 module.exports = {
-    getFruitsHistory,
-    getFruit,
-    addFruit,
-    deleteFruit
-}
+  getFruitsHistory,
+  getFruit,
+  addFruit,
+  deleteFruit,
+  uploadToBucket,
+};
--- routes/Fruit.js
+++ routes/Fruit.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const { addFruit, deleteFruit, getFruit } = require('../controllers/Fruits.controller');
+const Multer = require("multer");
+const { addFruit, deleteFruit, getFruit, uploadToBucket } = require('../controllers/Fruits.controller');
+
 const router = express.Router();
+
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    },
+});
 
 router.post('/add', addFruit);
 
 router.get('/delete/:id', deleteFruit)
 
 router.get('/:id', getFruit)
+
+router.post('/upload', multer.single("imgfile"), uploadToBucket);
 
 module.exports = router;
